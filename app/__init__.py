642acf31b01ca3e46d4da25c40bcd7ca5539334f
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-14 21:05:10 +0300

    update init file to use set configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from .config import DevConfig
 
 # Initializing the app
 app = Flask(__name__)
+
+# Configuration setup
+app.config.from_object(DevConfig)
+
 
 from app import views
