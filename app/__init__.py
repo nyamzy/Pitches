4b34970b7ea19e8a59b723b13ff7a8b5ef1f1b99
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-16 11:10:39 +0300

    create function for creating the app and register the main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask
-from .config import DevConfig
+from .config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
-# Initializing the app
-app = Flask(__name__)
+db = SQLAlchemy()
 
-# Configuration setup
-app.config.from_object(DevConfig)
+def create_app(config_name):
 
+    # Initializing the app
+    app = Flask(__name__)
 
-from app import views
+    # Configuration setup
+    app.config.from_object(config_options(config_name))
+
+    #Initializing flask extensions
+    db.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    
+    return app
