6ff77124ff44afd4676fb2872c3196592e47ed5f
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-17 21:42:32 +0300

    create tests for authentication

--- tests/test_user.py
+++ tests/test_user.py
@@ -6,4 +6,15 @@ class UserTest(unittest.TestCase):
     Test case to test behaviour of the User class
     '''
     def setUp(self):
-        self.new_user = User()
+        self.new_user = User(password = 'banana')
+
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.pass_secure is not None)
+
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('banana'))
+        
