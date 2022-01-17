fb44112a0e1242e69bb4c7e5c3e0997c61cf5ca4
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-17 12:54:40 +0300

    create view functions for jokes, vows and product categories

--- app/views.py
+++ app/views.py
@@ -6,4 +6,34 @@ def index():
     """
     Root page view function
     """
-    return render_template('index.html')
+    title = 'Quick Pitches'
+
+    return render_template('index.html', title = title)
+
+
+@app.route('/vows')
+def vows():
+    """
+    View page function for the vows
+    """
+    title = 'Vows'
+
+    return render_template('vows.html', title = title)
+
+@app.route('/product')
+def product():
+    """
+    View page function for product pitches
+    """
+    title = 'Product'
+
+    return render_template('product.html', title = title)
+
+@app.route('/jokes')
+def jokes():
+    """
+    View page function for the vows
+    """
+    title = 'Jokes'
+
+    return render_template('jokes.html', title = title)
