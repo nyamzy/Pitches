27aff1fea735fb538370d6d50a1ddf3a9f0c4de6
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-17 20:40:32 +0300

    initialize the database

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,10 @@ from ensurepip import bootstrap
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
     # Initializing the app
@@ -14,6 +16,7 @@ def create_app(config_name):
 
     # Initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
