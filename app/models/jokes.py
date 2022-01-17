88b91e5fba79ccf4735812f20d1287e28c441832
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-17 12:34:05 +0300

    create models for the different categories

---
+++ app/models/jokes.py
@@ -0,0 +1,8 @@
+class Jokes:
+    '''
+    Jokes class to define the objects
+    '''
+    def __init__(self, name, title, joke):
+        self.name = name
+        self.title = title
+        self.joke = joke
