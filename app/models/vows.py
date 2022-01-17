c04661842844c99fc7f01948f37c91eb7021c65b
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-17 12:45:13 +0300

    create vows model

---
+++ app/models/vows.py
@@ -0,0 +1,8 @@
+class Vows:
+    '''
+    Vows class to define the objects
+    '''
+    def __init__(self, name, title, vow):
+        self.name = name
+        self.title = title
+        self.vow = vow
