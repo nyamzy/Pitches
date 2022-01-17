aef519a54825097a2708a2daa75359aab1beb773
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-17 12:39:58 +0300

    create the product class

---
+++ app/models/product.py
@@ -0,0 +1,8 @@
+class Product:
+    '''
+    Product class to define the objects
+    '''
+    def __init__(self, name, title, product):
+        self.name = name
+        self.title = title
+        self.product = product
