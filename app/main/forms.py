aeea6e03b90c79db4e95f20756bcac57d23f629b
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-18 13:10:46 +0300

    update wtf validators

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,14 +2,13 @@ from tokenize import String
 from unittest import TextTestRunner
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import InputRequired
 
 class CommentForm(FlaskForm):
-    title = StringField('Comment title', validators=[Required()])
-    comment = TextAreaField('Your comment here', validators=[Required()])
+    title = StringField('Comment title', validators=[InputRequired()])
+    comment = TextAreaField('Your comment here', validators=[InputRequired()])
     submit = SubmitField('Post Comment')
     
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about yourself', validators=[Required()])
+    bio = TextAreaField('Tell us about yourself', validators=[InputRequired()])
     submit = SubmitField('Submit')
-    
