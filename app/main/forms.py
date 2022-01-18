c93df1141762158a68b90e1315047e325091812b
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-18 11:30:01 +0300

    create a profile page section

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,4 +1,5 @@
 from tokenize import String
+from unittest import TextTestRunner
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import Required
@@ -7,4 +8,8 @@ class CommentForm(FlaskForm):
     title = StringField('Comment title', validators=[Required()])
     comment = TextAreaField('Your comment here', validators=[Required()])
     submit = SubmitField('Post Comment')
+    
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about yourself', validators=[Required()])
+    submit = SubmitField('Submit')
     
