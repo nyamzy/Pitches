7f660db4d07b61033a9c529e69f04052a06c4444
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-21 01:24:43 +0300

    change configuration to production

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class ProdConfig(Config):
     """
     Production configuration child class
     """
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL1")
 
 class DevConfig(Config):
     """
